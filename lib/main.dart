a0c363b438a1915055ab92a277374a936b91e693
Mihail Paley <undeadlol1@mail.ru>
2021-04-16 21:31:36 +0500

    refactor: minor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:my_wins_today/TestButton.dart';
 import 'package:my_wins_today/screens/MainScreen.dart';
 import 'package:my_wins_today/widgets/CreateWinForm.dart';
@@ -18,7 +17,7 @@ final List<Win> wins = [
     createdAt: 123,
   ),
   new Win(
-    title: 'Got worked on the project',
+    title: 'Worked on the project',
     id: '123',
     userId: '123',
     updatedAt: 123,
@@ -66,35 +65,35 @@ class MyApp extends StatelessWidget {
               child: Text(k.text(label: 'Text', initial: 'Flat button')),
             ),
           ),
-          Story(
-            section: 'Buttons',
-            name: 'Raised button',
-            // ignore: deprecated_member_use
-            builder: (_, k) => RaisedButton(
-              onPressed:
-                  k.boolean(label: 'Enabled', initial: true) ? () {} : null,
-              color: k.options(
-                label: 'Color',
-                initial: Colors.deepOrange,
-                options: const [
-                  Option('Red', Colors.deepOrange),
-                  Option('Green', Colors.teal),
-                ],
-              ),
-              mouseCursor: k.options(
-                label: 'Mouse Cursor',
-                initial: null,
-                options: const [
-                  Option('Basic', SystemMouseCursors.basic),
-                  Option('Click', SystemMouseCursors.click),
-                  Option('Forbidden', SystemMouseCursors.forbidden),
-                ],
-              ),
-              elevation: k.slider(label: 'Elevation', initial: 0, max: 20),
-              textColor: Colors.white,
-              child: Text(k.text(label: 'Text', initial: 'Raised button')),
-            ),
-          ),
+          // Story(
+          //   section: 'Buttons',
+          //   name: 'Raised button',
+          //   // ignore: deprecated_member_use
+          //   builder: (_, k) => RaisedButton(
+          //     onPressed:
+          //         k.boolean(label: 'Enabled', initial: true) ? () {} : null,
+          //     color: k.options(
+          //       label: 'Color',
+          //       initial: Colors.deepOrange,
+          //       options: const [
+          //         Option('Red', Colors.deepOrange),
+          //         Option('Green', Colors.teal),
+          //       ],
+          //     ),
+          //     mouseCursor: k.options(
+          //       label: 'Mouse Cursor',
+          //       initial: null,
+          //       options: const [
+          //         Option('Basic', SystemMouseCursors.basic),
+          //         Option('Click', SystemMouseCursors.click),
+          //         Option('Forbidden', SystemMouseCursors.forbidden),
+          //       ],
+          //     ),
+          //     elevation: k.slider(label: 'Elevation', initial: 0, max: 20),
+          //     textColor: Colors.white,
+          //     child: Text(k.text(label: 'Text', initial: 'Raised button')),
+          //   ),
+          // ),
           Story.simple(name: 'Button for testing', child: const TestButton()),
           Story.simple(
             name: 'Input field',

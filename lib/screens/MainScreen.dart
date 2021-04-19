f0ad30b385652939d9fbd9dac03733003a7493fb
undeadlol1 <undeadlol1@mail.ru>
2021-04-20 00:30:55 +0500

    feat(firebase): add current user state to main screen

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:my_wins_today/widgets/WinsList.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../entities/Win.dart';
+import '../widgets/layout.dart';
+import '../widgets/WinsList.dart';
 
 class MainScreen extends StatelessWidget {
   final List<Win> myWinsToday;
@@ -9,8 +11,34 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('123')),
-        body: WinsList(wins: this.myWinsToday));
+    return StreamBuilder<User?>(
+        stream: _stream(),
+        builder: (_, snapshot) {
+          final auth = snapshot.data;
+          final isAuthLoading =
+              snapshot.connectionState == ConnectionState.waiting;
+
+          _printAuthInfo(auth);
+
+          return Layout(
+            title: 'Список побед',
+            body: WinsList(
+              wins: this.myWinsToday,
+              isLoading: isAuthLoading,
+            ),
+          );
+        });
+  }
+
+  Stream<User?> _stream() {
+    return FirebaseAuth.instance.authStateChanges();
+  }
+
+  void _printAuthInfo(User? auth) {
+    if (auth == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
   }
 }

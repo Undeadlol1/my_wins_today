ab348e20d8f46952de222d022381c05da19dd176
undeadlol1 <undeadlol1@mail.ru>
2021-04-19 22:28:23 +0500

    feat(firebase): :wrench: add firebase boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'StoriesList.dart';
 import 'screens/MainScreen.dart';
 
-void main() => runApp(const Application());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(Application());
+}
 
 class Application extends StatelessWidget {
-  const Application({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    const _isStorybookEnabled = true;
-    return _isStorybookEnabled
-        ? StoriesList()
-        : MaterialApp(
+    const _isStorybookEnabled = false;
+    if (_isStorybookEnabled) {
+      return StoriesList();
+    }
+
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print('Something were thrown during Firebase initialisation.');
+          print(snapshot.error);
+          return MaterialApp(
+            home: Center(
+              child: Text('Firebase Error.'),
+            ),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
             home: MainScreen(myWinsToday: []),
           );
+        }
+
+        return MaterialApp(
+          home: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
   }
 }

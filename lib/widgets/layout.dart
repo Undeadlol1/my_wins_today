ab708dfa580c508b17c3a3ec2800003521dced03
undeadlol1 <undeadlol1@mail.ru>
2021-04-21 00:12:41 +0500

    feat: add login logout button to layout

--- lib/widgets/layout.dart
+++ lib/widgets/layout.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:my_wins_today/screens/sign_in_screen.dart';
+import 'package:my_wins_today/streams/viewer_stream.dart';
+import 'package:my_wins_today/use_cases/log_out.dart';
 
 class Layout extends StatelessWidget {
   final String title;
@@ -30,24 +33,40 @@ class Layout extends StatelessWidget {
   }
 
   Widget _linksList(BuildContext context) {
-    return ListView(
-      children: [
-        ListTile(
-          title: TextButton(
-            onPressed: () {},
-            child: ListTile(
-              onTap: () => Navigator.pushNamed(
-                context,
-                SignInScreen.path,
-              ),
-              title: Text(
-                'Войти',
-                textAlign: TextAlign.center,
-              ),
-            ),
+    return StreamBuilder<User?>(
+      stream: viewerStream(),
+      builder: (context, snapshot) {
+        final bool isViewerSignedIn = snapshot.data != null;
+        final bool isAuthLoaded =
+            snapshot.connectionState == ConnectionState.active;
+        return ListView(
+          children: [
+            if (isAuthLoaded) _loginOrLogoutButton(context, isViewerSignedIn)
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _loginOrLogoutButton(BuildContext context, bool isViewerSignedIn) {
+    return ListTile(
+      title: TextButton(
+        onPressed: () {},
+        child: ListTile(
+          onTap: () async {
+            isViewerSignedIn
+                ? await logOut()
+                : Navigator.pushNamed(
+                    context,
+                    SignInScreen.path,
+                  );
+          },
+          title: Text(
+            isViewerSignedIn ? 'Выйти' : 'Войти',
+            textAlign: TextAlign.center,
           ),
-        )
-      ],
+        ),
+      ),
     );
   }
 }

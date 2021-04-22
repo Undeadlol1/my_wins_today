629ef10d52da2da9026dfd6ca21e851aabf245d3
undeadlol1 <undeadlol1@mail.ru>
2021-04-23 02:03:35 +0500

    feat: add getViewerId future

--- lib/states/viewer_state.dart
+++ lib/states/viewer_state.dart
@@ -3,16 +3,19 @@ import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class ViewerState extends GetxController {
   User? viewer;
+  String? userId;
   bool isLoading = true;
 
   void login(User user) {
     viewer = user;
     isLoading = false;
+    userId = user.uid;
     update();
   }
 
   void logout() {
     viewer = null;
+    userId = null;
     isLoading = false;
     update();
   }

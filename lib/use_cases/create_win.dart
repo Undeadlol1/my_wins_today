400b456f3a27c1f01e4b6bb19c380d80fbb2c149
undeadlol1 <undeadlol1@mail.ru>
2021-04-24 22:15:09 +0500

    refactor: minor

--- lib/use_cases/create_win.dart
+++ lib/use_cases/create_win.dart
@@ -2,9 +2,9 @@ import 'package:my_wins_today/entities/Win.dart';
 import 'package:my_wins_today/DTOs/win_create_dto.dart';
 import 'package:my_wins_today/repositories/create_win_repository.dart';
 
-Future<Win> createWin(WinCreateDTO winPayload) async {
+Future<Win> createWin(WinCreateDTO winDTO) async {
   try {
-    return await createWinRepository(winPayload);
+    return await createWinRepository(winDTO);
   } on Exception catch (error) {
     throw error;
   }

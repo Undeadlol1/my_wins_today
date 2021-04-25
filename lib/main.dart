0b0aa77b6a8185aa0f2ea1e1df667eaa1868021e
undeadlol1 <undeadlol1@mail.ru>
2021-04-25 23:57:30 +0500

    feat: use "initialRoute" instad of "home"

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:my_wins_today/global_dependencies.dart';
 import 'package:my_wins_today/screens/create_win_screen.dart';
+import 'package:my_wins_today/screens/main_screen.dart';
 import 'package:my_wins_today/screens/main_screen_container.dart';
 
 import 'screens/sign_in_screen.dart';
@@ -23,13 +24,14 @@ class Application extends StatelessWidget {
     return GlobalDependencies(
       child: GetMaterialApp(
         routes: _buildRoutes(),
-        home: MainScreenContainer(),
+        initialRoute: MainScreen.path,
       ),
     );
   }
 
   Map<String, Widget Function(BuildContext)> _buildRoutes() {
     return {
+      MainScreen.path: (context) => MainScreenContainer(),
       SignInScreen.path: (context) => SignInScreen(),
       CreateWinScreen.path: (context) => CreateWinScreen(wins: []),
     };

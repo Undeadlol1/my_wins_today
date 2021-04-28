7c192a5175dce477cbe52eddebc14c03e76ae4eb
undeadlol1 <undeadlol1@mail.ru>
2021-04-29 02:01:37 +0500

    feat: assign custom id to document during win creation

--- lib/repositories/create_win_repository.dart
+++ lib/repositories/create_win_repository.dart
@@ -1,27 +1,29 @@
 import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:my_wins_today/entities/Win.dart';
 import 'package:my_wins_today/DTOs/win_create_dto.dart';
 import 'package:my_wins_today/utilities/generateId.dart';
 
-final CollectionReference winsCollection =
-    FirebaseFirestore.instance.collection('wins');
+final winsCollection = FirebaseFirestore.instance.collection('wins');
 
 Future<Win> createWinRepository(WinCreateDTO winPayload) async {
   log('createWinRepository() is called.');
 
+  final documentId = generateId();
   final currentTimeInMilliseconds = DateTime.now().millisecondsSinceEpoch;
-
-  final Win winToBeCreated = Win(
-    id: generateId(),
+  final winToBeCreated = Win(
+    id: documentId,
     title: winPayload.title,
     userId: winPayload.userId,
     createdAt: currentTimeInMilliseconds,
     updatedAt: currentTimeInMilliseconds,
   );
 
-  await winsCollection.add(winToBeCreated.toMap()).catchError((error) {
+  await winsCollection
+      .doc(documentId)
+      .set(winToBeCreated.toMap())
+      .catchError((error) {
     log("Failed to add a win: $error");
     throw error;
   });

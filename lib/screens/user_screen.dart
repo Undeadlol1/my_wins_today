bac4d30e335848b63aa6435fde4c336c774f4f05
Mihail Paley <undeadlol1@mail.ru>
2021-05-08 08:14:54 +0500

    feat: :construction: adding UserScreen

---
+++ lib/screens/user_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:my_wins_today/entities/Win.dart';
+import 'package:my_wins_today/widgets/layout.dart';
+import 'package:my_wins_today/widgets/wins_list_container.dart';
+
+class UserScreen extends StatelessWidget {
+  static const path = '/user';
+
+  final bool isLoading;
+  final List<Win> winsToday;
+
+  const UserScreen({
+    Key? key,
+    this.isLoading = false,
+    required this.winsToday,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Layout(
+      title: 'Список побед',
+      body: WinsListConntainer(
+        wins: this.winsToday,
+        isLoading: this.isLoading,
+      ),
+    );
+  }
+}
